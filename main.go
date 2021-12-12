87ef50b167bfb90c425bd8fea1657314dcdc237c
Irochka <bogdan315991@gmail.com>
2021-12-12 19:33:07 +0200

    add chanel

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	qt := 20
 	floes1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	floes2 := []int{1, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
-	ch := make(chan models.Person, 50)
+	ch := make(chan models.Person, qt)
 	for i := 0; i < qt; i++ {
 		go people.GenMorningPeople(ch)
 	}
